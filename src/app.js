21f50a221111f2b71e7150d18fae0d335a9d2721
SynCROSS <kuuhaku3125@gmail.com>
2022-11-06 10:08:58 +0900

    Use ES5

--- src/app.js
+++ src/app.js
@@ -1,22 +1,18 @@
-window.onload = function () {
-  // TODO Practice #1 - Add Global Component 'todo-footer'
-  Vue.component('todo-footer', {
-    template: '<p>This is global child component</p>',
-  });
-
-  // eslint-disable-next-line no-new
-  new Vue({
-    el: '#app',
-    data() {
-      return {
-        message: 'This is a parent component',
-      };
-    },
-    // TODO Practice #2 - Add Local Component 'todo-list'
-    components: {
-      'todo-list': {
-        template: '<p>This is another local child component</p>',
-      },
-    },
-  });
+window.onload = function onload() {
+    Vue.component("todo-footer", {
+        template: "<p>This is global child component</p>"
+    });
+    new Vue({
+        el: "#app",
+        data: function data() {
+            return {
+                message: "This is a parent component"
+            };
+        },
+        components: {
+            "todo-list": {
+                template: "<p>This is another local child component</p>"
+            }
+        }
+    });
 };
