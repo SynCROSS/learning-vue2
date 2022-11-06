f1202c3685725c8c61e11330990408e1c6513fb2
SynCROSS <kuuhaku3125@gmail.com>
2022-11-06 18:29:21 +0900

    Use Props To Pass Value To Child

--- src/app.js
+++ src/app.js
@@ -2,31 +2,22 @@ window.onload = function onload() {
   if (typeof Vue === 'undefined') {
     return;
   }
-
-  var myComponent1 = {
-    template: '<p>1st Local Component: {{data1}}</p>',
-    data: function data() {
-      return {
-        data1: 100,
-      };
+  Vue.component('child-component', {
+    props: {
+      message: {
+        type: [String, Number],
+        required: true,
+      }
     },
-  };
-
-  var myComponent2 = {
-    template: '<p>2nd Local Component: {{data2}}</p>',
-    data: function data() {
-      return {
-        data2: myComponent1.data().data1, // ? Why data2 is 100?
-      };
-    },
-  };
-
+    template: '<p>{{message}}</p>',
+  });
   // eslint-disable-next-line no-new
   new Vue({
     el: '#app',
-    components: {
-      'my-component1': myComponent1,
-      'my-component2': myComponent2,
-    },
+    data: function data() {
+      return {
+        message: '"Hello Vue!" From Parent component'
+      }
+    }
   });
 };
