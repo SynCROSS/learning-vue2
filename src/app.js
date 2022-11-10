965b4e19ba2fcbe8d5bd475fe06ab6e59ac3deca
SynCROSS <kuuhaku3125@gmail.com>
2022-11-10 20:12:45 +0900

    Use Vue Router

--- src/app.js
+++ src/app.js
@@ -1,34 +1,112 @@
-window.onload = function onload() {
-  if (typeof Vue === "undefined") {
+var loadScript = function loadScript(src) {
+  return new Promise(function (resolve, reject) {
+    var script = document.createElement('script');
+    script.src = src;
+    script.onload = function () {
+      return resolve(script);
+    };
+    script.onerror = function () {
+      return reject(new Error('Script load error for '.concat(src)));
+    };
+    document.body.append(script);
+  });
+};
+var _arrayLikeToArray = function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) {
+    len = arr.length;
+  }
+  var arr2 = new Array(len);
+  for (var i = 0; i < len; i++) {
+    arr2[i] = arr[i];
+  }
+  return arr2;
+};
+var _arrayWithoutHoles = function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) {
+    return _arrayLikeToArray(arr);
+  }
+};
+var _iterableToArray = function _iterableToArray(iter) {
+  if (
+    (typeof Symbol !== 'undefined' && iter[Symbol.iterator] != null) ||
+    iter['@@iterator'] != null
+  ) {
+    return Array.from(iter);
+  }
+};
+var _nonIterableSpread = function _nonIterableSpread() {
+  throw new TypeError(
+    'Invalid attempt to spread non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.',
+  );
+};
+var _toConsumableArray = function _toConsumableArray(arr) {
+  return (
+    _arrayWithoutHoles(arr) ||
+    _iterableToArray(arr) ||
+    _unsupportedIterableToArray(arr) ||
+    _nonIterableSpread()
+  );
+};
+var _unsupportedIterableToArray = function _unsupportedIterableToArray(
+  o,
+  minLen,
+) {
+  if (!o) {
     return;
   }
-  // TODO Practice #1 Add sibling-component with template and options
-  Vue.component('sibling-component', {
-    template: '<p>anotherMessage: {{anotherMessage}}</p>',
-    props: {
-      anotherMessage: {
-        type: String,
-        required: true,
+  if (typeof o === 'string') {
+    return _arrayLikeToArray(o, minLen);
+  }
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === 'Object' && o.constructor) {
+    n = o.constructor.name;
+  }
+  if (n === 'Map' || n === 'Set') {
+    return Array.from(n);
+  }
+  if (n === 'Arguments' || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) {
+    return _arrayLikeToArray(o, minLen);
+  }
+};
+window.onload = function onload() {
+  loadScript('https://cdn.jsdelivr.net/npm/vue@2.5.2/dist/vue.js')
+    .then(function () {
+      return loadScript(
+        'https://unpkg.com/vue-router@3.0.1/dist/vue-router.js',
+      );
+    })
+    .then(function () {
+      if (typeof Vue === 'undefined' || typeof VueRouter === 'undefined') {
+        return;
       }
-    }
-  })
-  Vue.component("child-component", {
-    template: '<p>message: {{message}}</p>',
-    props: {
-      message: {
-        type: String,
-        required: true,
-      }
-    }
-  });
-  new Vue({
-    el: "#app",
-    data: function data() {
-      return {
-        message: '"Hello Vue!" From Parent component',
-        // TODO Practice #2 Add 'anotherMessage' in data
-        anotherMessage: '"Hello Vue!" From Parent component'.split('').reverse().join('')
-      }
-    },
-  });
+      var Main = {
+        template: '<b>main</b>',
+      };
+      var Login = {
+        template: '<b>login</b>',
+      };
+      var routes = new Map([
+        [
+          'main',
+          {
+            path: '/main',
+            component: Main,
+          },
+        ],
+        [
+          'login',
+          {
+            path: '/login',
+            component: Login,
+          },
+        ],
+      ]);
+      var router = new VueRouter({
+        routes: _toConsumableArray(routes.values()),
+      });
+      new Vue({
+        router: router,
+      }).$mount('#app');
+    })
+    .catch(console.error);
 };
